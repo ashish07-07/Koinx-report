cc3493165129fa7352b78768b4aaeaf78d318aef
B <bkashishh07@gmail.com>
2024-08-12 18:46:45 +0530

    Added the cors package

--- backend/dist/routes/balance.js
+++ backend/dist/routes/balance.js
@@ -1,7 +1,4 @@
 "use strict";
-// import express, { response } from "express";
-// import { Request, Response } from "express";
-// import Trade from "../db";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,43 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const router = express.Router();
-// router.use(express.json());
-// router.post("/balencesummary", async function (req: Request, res: Response) {
-//   let body = req.body;
-//   let timestamp = new Date(body.timestamp);
-//   console.log("Parsed Timestamp:", timestamp);
-//   try {
-//     const trades = await Trade.find({
-//       date: { $lte: timestamp },
-//     });
-//     console.log("Trades found:", trades);
-//     const balances: { [key: string]: number } = {};
-//     trades.forEach(function (val) {
-//       const { Netamount, Trasactiontype, Cryptocurrency } = val;
-//       console.log(`${Cryptocurrency} ${Netamount} ${Trasactiontype}`);
-//       if (Cryptocurrency && Netamount && Trasactiontype) {
-//         if (!balances[Cryptocurrency]) {
-//           balances[Cryptocurrency] = 0;
-//         }
-//         balances[Cryptocurrency] +=
-//           Trasactiontype === "Buy" ? Netamount : -Netamount;
-//       }
-//     });
-//     console.log("Calculated Balances:", balances);
-//     return res.status(201).json({
-//       memainingasset: balances,
-//     });
-//   } catch (error) {
-//     console.error("Error fetching trades:", error);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-// export default router;
 const express_1 = __importDefault(require("express"));
 const db_1 = __importDefault(require("../db"));
+const cors_1 = __importDefault(require("cors"));
 const router = express_1.default.Router();
 router.use(express_1.default.json());
+router.use((0, cors_1.default)());
 router.post("/balencesummary", function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         let body = req.body;
--- backend/src/routes/balance.ts
+++ backend/src/routes/balance.ts
@@ -1,60 +1,14 @@
-// import express, { response } from "express";
-// import { Request, Response } from "express";
-// import Trade from "../db";
-
-// const router = express.Router();
-
-// router.use(express.json());
-
-// router.post("/balencesummary", async function (req: Request, res: Response) {
-//   let body = req.body;
-//   let timestamp = new Date(body.timestamp);
-
-//   console.log("Parsed Timestamp:", timestamp);
-
-//   try {
-//     const trades = await Trade.find({
-//       date: { $lte: timestamp },
-//     });
-
-//     console.log("Trades found:", trades);
-
-//     const balances: { [key: string]: number } = {};
-
-//     trades.forEach(function (val) {
-//       const { Netamount, Trasactiontype, Cryptocurrency } = val;
-//       console.log(`${Cryptocurrency} ${Netamount} ${Trasactiontype}`);
-
-//       if (Cryptocurrency && Netamount && Trasactiontype) {
-//         if (!balances[Cryptocurrency]) {
-//           balances[Cryptocurrency] = 0;
-//         }
-
-//         balances[Cryptocurrency] +=
-//           Trasactiontype === "Buy" ? Netamount : -Netamount;
-//       }
-//     });
-
-//     console.log("Calculated Balances:", balances);
-
-//     return res.status(201).json({
-//       memainingasset: balances,
-//     });
-//   } catch (error) {
-//     console.error("Error fetching trades:", error);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// export default router;
-
 import express, { response } from "express";
 import { Request, Response } from "express";
 import Trade from "../db";
 
+import cors from "cors";
+
 const router = express.Router();
 
 router.use(express.json());
+
+router.use(cors());
 
 router.post("/balencesummary", async function (req: Request, res: Response) {
   let body = req.body;
