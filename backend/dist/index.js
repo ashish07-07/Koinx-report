e84d57f07f0ea6b405442df664e6a53e4bacaa79
B <bkashishh07@gmail.com>
2024-08-12 18:48:14 +0530

    Update index.js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,11 +7,13 @@ const express_1 = __importDefault(require("express"));
 const prolo_1 = __importDefault(require("./routes/prolo"));
 const balance_1 = __importDefault(require("./routes/balance"));
 const profit_loss_1 = __importDefault(require("./routes/profit-loss"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/reports", prolo_1.default);
 app.use("/asset", balance_1.default);
 app.use("/profitlosscalculatot", profit_loss_1.default);
+app.use((0, cors_1.default)());
 app.listen(3000, function () {
     console.log("server listening on port");
 });
